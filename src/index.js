7e432e627700b0f12404dddb444eec74a5f72ebe
Sarah <spfohman@email.essex.edu>
2021-09-29 11:50:32 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 console.log('%c HI', 'color: firebrick')
+function fetchDogs(){
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+    fetch(imgUrl)
+    .then((response)=>{
+        return response.json()
+    })
+    .then((json)=>addImages(json))
+    
+}
+
+function addImages(images){
+    const added = document.getElementById('dog-image-container')
+    images.value.forEach(Image =>{
+        const img= document.createElement('img');
+        img.src = Image;
+        added.appendChild(img)
+    });
+}
+document.addEventListener('DOMContentLoaded', ()=>{
+    fetchDogs()
+})
