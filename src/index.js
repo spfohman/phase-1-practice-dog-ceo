efa6a9b50ac37f690460c790169412335bb90263
Sarah <spfohman@email.essex.edu>
2021-09-30 10:24:06 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,59 @@
+
 console.log('%c HI', 'color: firebrick')
-function fetchDogs(){
-    const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetchImages();
+    fetchDogBreeds();
+    changeColor();
+    chooseLetter();
+})
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+function fetchImages() {
     fetch(imgUrl)
-    .then((response)=>{
-        return response.json()
-    })
-    .then((json)=>addImages(json))
-    
+    .then(response => response.json())
+    .then(data => {data.message.forEach(image => renderImages(image))})
+
 }
 
-function addImages(images){
-    const added = document.getElementById('dog-image-container')
-    images.value.forEach(Image =>{
-        const img= document.createElement('img');
-        img.src = Image;
-        added.appendChild(img)
-    });
+function renderImages(data){
+    const imageLocal = document.getElementById('dog-image-container');
+    const image = document.createElement('img');
+    image.src = data;
+    console.log(image);
+    imageLocal.appendChild(image);
+   
 }
-document.addEventListener('DOMContentLoaded', ()=>{
-    fetchDogs()
-})
+const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+function fetchDogBreeds(){
+    fetch(breedUrl)
+    .then(response=>response.json())
+    .then(data=>renderBreed(data))
+}
+
+function renderBreed(data){
+    const breedLocation = document.getElementById('dog-breeds');
+    for(let i in data.message){
+        console.log(i);
+        const li = document.createElement('li');
+        li.innerText = i;
+        breedLocation.appendChild(li);
+    }
+}
+
+function changeColor(event){
+    event.target.style.color= "purple";
+}
+function chooseLetter(data){
+    const letter = document.querySelector('#breed-dropdown');
+    letter.addEventListener('change', function(event){
+        for(let i in data.event){
+            let choice = letter.value;
+            if(i.startsWith(choice)){
+                console.log(i);
+            }
+        }
+        
+    })
+
+}
